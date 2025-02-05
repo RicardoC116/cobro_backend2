a5676b0c0474d5c33b9446d105f845a59186219a
RicardoC116 <contrerasr1136@gmail.com>
2025-02-05 15:55:30 -0600

    Modificacion de tablas

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ db.authenticate()
 
     // Sincronizar la base de datos
     return db
-      .sync({ alter: false })
+      .sync({ alter: true })
       .then(() => {
         console.log("Base de datos sincronizada correctamente.");
       })
